33c4ffdf5778af58014de5957f64732935fe1d02
Cristian Crețu <crisemcr@gmail.com>
2023-10-23 10:30:30 +0300

    feat: reponses

--- main.rs
+++ main.rs
@@ -1,13 +1,42 @@
 use std::{
-    io::Write,
+    io::{self, BufRead, Write},
     net::{TcpListener, TcpStream},
 };
 
-fn handle_connection(mut stream: TcpStream) {
-    println!("Accepted Connection");
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    stream.write(response.as_bytes()).unwrap();
+const SUCCESS_RESPONSE: &str = "HTTP/1.1 200 OK\r\n\r\n";
+const NOT_FOUND_RESPONSE: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+
+enum Response {
+    Success,
+    NotFound,
+}
+
+impl Response {
+    fn to_string(&self) -> String {
+        match self {
+            Response::Success => SUCCESS_RESPONSE.to_string(),
+            Response::NotFound => NOT_FOUND_RESPONSE.to_string(),
+        }
+    }
+}
+
+fn send_response(mut stream: TcpStream, response: Response) {
+    stream.write(response.to_string().as_bytes()).unwrap();
     stream.flush().unwrap();
+}
+
+fn handle_client_connection(stream: TcpStream) {
+    let mut reader = io::BufReader::new(&stream);
+    let received: Vec<u8> = reader.fill_buf().unwrap().to_vec();
+
+    let x = String::from_utf8(received).unwrap();
+
+    let path = x.split(" ").collect::<Vec<&str>>()[1];
+
+    match path {
+        "/" => send_response(stream, Response::Success),
+        _ => send_response(stream, Response::NotFound),
+    }
 }
 
 fn main() {
@@ -15,7 +44,7 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => handle_connection(stream),
+            Ok(stream) => handle_client_connection(stream),
             Err(e) => {
                 println!("error: {}", e);
             }
