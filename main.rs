3ce90c64e544aead6c9c4da582afa461abfe0563
Cristian Crețu <crisemcr@gmail.com>
2023-10-23 21:51:50 +0300

    feat: add new func

--- main.rs
+++ main.rs
@@ -3,8 +3,8 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
-const SUCCESS_RESPONSE: &str = "HTTP/1.1 200 OK\r\n\r\n";
-const NOT_FOUND_RESPONSE: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+const SUCCESS_RESPONSE: &str = "HTTP/1.1 200 OK\r\n";
+const NOT_FOUND_RESPONSE: &str = "HTTP/1.1 404 NOT FOUND\r\n";
 
 enum Response {
     Success,
@@ -20,12 +20,27 @@ impl Response {
     }
 }
 
-fn send_response(mut stream: TcpStream, response: Response) {
-    stream.write(response.to_string().as_bytes()).unwrap();
+fn send_response(stream: &mut TcpStream, response: Response, content: Option<String>) {
+    let response_str = response.to_string();
+    let content_str = content.unwrap_or_default();
+    let full_response;
+
+    if content_str.is_empty() {
+        full_response = format!("{}\r\n", response_str);
+    } else {
+        full_response = format!(
+            "{}Content-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n\r\n",
+            response_str,
+            content_str.len(),
+            content_str
+        );
+    }
+
+    stream.write_all(full_response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
 
-fn handle_client_connection(stream: TcpStream) {
+fn handle_client_connection(mut stream: TcpStream) {
     let mut reader = io::BufReader::new(&stream);
     let received: Vec<u8> = reader.fill_buf().unwrap().to_vec();
 
@@ -34,8 +49,22 @@ fn handle_client_connection(stream: TcpStream) {
     let path = x.split(" ").collect::<Vec<&str>>()[1];
 
     match path {
-        "/" => send_response(stream, Response::Success),
-        _ => send_response(stream, Response::NotFound),
+        "/" => send_response(&mut stream, Response::Success, None),
+        "/user-agent" => {
+            let user_agent = reader
+                .lines()
+                .find(|line| line.as_ref().unwrap().starts_with("User-Agent: "))
+                .unwrap()
+                .unwrap()
+                .replace("User-Agent: ", "");
+            send_response(&mut stream, Response::Success, Some(user_agent))
+        }
+        _ if path.starts_with("/echo") => send_response(
+            &mut stream,
+            Response::Success,
+            Some(path.split("/echo/").collect::<Vec<&str>>()[1].to_string()),
+        ),
+        _ => send_response(&mut stream, Response::NotFound, None),
     }
 }
 
